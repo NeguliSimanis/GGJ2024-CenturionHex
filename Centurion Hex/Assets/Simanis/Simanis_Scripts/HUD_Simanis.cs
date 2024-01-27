f695a8e9f3249fcc67191f853a81aa17e2d02a46
NeguliSimanis <simanism@inbox.lv>
2024-01-27 15:20:37 +0200

    scene changes, ui improvements

--- Centurion Hex/Assets/Simanis/Simanis_Scripts/HUD_Simanis.cs
+++ Centurion Hex/Assets/Simanis/Simanis_Scripts/HUD_Simanis.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class HUD_Simanis : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public CenturionGame centurionGame;
+
+    public TextMeshProUGUI generalOrGovernorText;
+    public string team0String = "-Team 1-";
+    public string team1String = "-Team 2-";
+    public string govString = "Governor Turn";
+    public string generalString = "General Turn";
+    public Color generalTurnColor;
+    public Color govTurnColor;
+    public TextMeshProUGUI teamTurnText;
+
+    public TextMeshProUGUI goldTeam0;
+    public TextMeshProUGUI goldTeam1;
+
+    public TextMeshProUGUI vicPointsTeam0;
+    public TextMeshProUGUI vicPointsTeam1;
+
+    public void UpdateTeamWealth()
     {
-        
+        goldTeam0.text = centurionGame.Teams[0].Gold.ToString();
+        goldTeam1.text = centurionGame.Teams[1].Gold.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateTurnText()
     {
-        
+        // general
+        if (centurionGame.GeneralMove)
+        {
+            generalOrGovernorText.text = generalString;
+            generalOrGovernorText.color = generalTurnColor;
+        }
+        // governor
+        else
+        {
+            generalOrGovernorText.text = govString;
+            generalOrGovernorText.color = govTurnColor;
+        }
+        // team 1
+        if (centurionGame.RedMove)
+        {
+            teamTurnText.text = team0String;
+        }
+        // Team 2
+        else
+        {
+            teamTurnText.text = team1String;
+        }
     }
 }
